550efa3ab86d99dca7eadf4ad2367a9e51af526d
Zahar <60888814+ZaharAmbrozyak@users.noreply.github.com>
2026-03-28 17:06:01 +0200

    Stage 3

--- cs401-week8-assignment-2/Program.cs
+++ cs401-week8-assignment-2/Program.cs
@@ -6,8 +6,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        var file = new FileResource("input.txt");
-        var network = new NetworkResource("kse.test.ua");
+        var file1 = new FileResource("input.txt");
+        var file2 = new FileResource("test.txt");
+        var network1 = new NetworkResource("kse.test.ua");
+        var network2 = new NetworkResource("ucu.test.ua");
+
+        var manager = new ResourceManager<Resource>();
+        
+        manager.Add(file1);
+        manager.Add(file2);
+        manager.Add(network1);
+        manager.Add(network2);
+        
+        manager.OpenAll();
+
+        using (var newResource = new FileResource("data.txt"))
+        {
+            newResource.Open();
+        }
+        
+        manager.CloseAll();
+        
         
     }
 }
