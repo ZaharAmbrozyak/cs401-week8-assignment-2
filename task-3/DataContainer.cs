eb276f699b7168cf3070e9da7f4488d02bd0c24b
Zahar <60888814+ZaharAmbrozyak@users.noreply.github.com>
2026-03-28 17:29:13 +0200

    Stage 2

---
+++ task-3/DataContainer.cs
@@ -0,0 +1,22 @@
+namespace task_3;
+
+public class DataContainer<T> where T: DataItem
+{
+    private readonly List<T> _data = [];
+
+    public void AddItem(T item)
+    {
+        _data.Add(item);
+    }
+
+    public int GetTotalSize()
+    {
+        var totalSize = 0;
+        foreach (var item in _data)
+        {
+            totalSize += item.GetSize();
+        }
+
+        return totalSize;
+    }
+}
